36f70db923449f85acbbd7163fdd584f014db172
wesfly <wesleydeanford@yahoo.com>
2025-02-03 21:02:34 +0100

    added comments and formatting

--- frequency-analyzer.py
+++ frequency-analyzer.py
@@ -8,18 +8,23 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 counter_list = []
 
+# character counter
 for i in range(0, len(alphabet)):
     count = string.count(alphabet[i])
     counter_list.extend([count])
 
-print("Length of string:", len(string))
 full_list = dict(zip(alphabet, counter_list))
 
-print(full_list)
+
 word_length_counter = 0
 words = string.split(" ")
+
+# average word length calculator
 for i in range(0 ,len(words)):
     word_length_counter += len(words[i])
 
 average_word_length = word_length_counter / len(words)
+
+print("Length of string:", len(string))
+print(full_list)
 print("Average word length", average_word_length)
